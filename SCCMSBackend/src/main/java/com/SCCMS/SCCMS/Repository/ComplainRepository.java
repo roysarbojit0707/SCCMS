6731f0581a4060ce93a239684d67506de5c45c68
Sarbojit Roy <sarbojitsroy7409@gmail.com>
2025-07-14 22:04:46 +0530

    add a interface for complain repository

--- SCCMSBackend/src/main/java/com/SCCMS/SCCMS/Repository/ComplainRepository.java
+++ SCCMSBackend/src/main/java/com/SCCMS/SCCMS/Repository/ComplainRepository.java
@@ -1,8 +1,9 @@
 package com.SCCMS.SCCMS.Repository;
 
+import com.SCCMS.SCCMS.Entity.ComplainEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ComplainEntity extends JpaRepository<ComplainEntity,String> {
+public interface ComplainRepository extends JpaRepository<ComplainEntity,String> {
 }
