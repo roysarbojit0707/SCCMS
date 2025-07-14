7e802273b2d1c8d37ee246a0d040bb71da53fa7a
Sarbojit Roy <sarbojitsroy7409@gmail.com>
2025-07-14 22:03:36 +0530

    add the controller to register complain

---
+++ SCCMSBackend/src/main/java/com/SCCMS/SCCMS/Controller/ComplainController.java
@@ -0,0 +1,32 @@
+package com.SCCMS.SCCMS.Controller;
+
+import com.SCCMS.SCCMS.DTO.ComplainDTO;
+import com.SCCMS.SCCMS.Service.ComplainService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/complain")
+public class ComplainController {
+    @Autowired
+    private ComplainService complainService;
+    @PostMapping("/registercomplain")
+    public Map<String,Boolean> RegisterComplain(@RequestBody ComplainDTO complainDTO){
+        String complainToken = complainService.generateComplainToken();
+        String fullName = complainDTO.getFullName();
+        String emailAddress = complainDTO.getEmailAddress();
+        String phoneNumber = complainDTO.getPhoneNumber();
+        String governmentIDType = complainDTO.getGovernmentIDType();
+        String building = complainDTO.getBuilding();
+        String apartmentNumber = complainDTO.getApartmentNumber();
+        String department = complainDTO.getDepartment();
+        String complain = complainDTO.getComplain();
+        boolean isComplainRegister = complainService.ComplainRegister(complainToken,fullName,emailAddress,phoneNumber,governmentIDType,building,apartmentNumber,department,complain);
+        return Map.of("isComplainRegister",isComplainRegister);
+    }
+}
